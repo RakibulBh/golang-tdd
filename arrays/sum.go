5f966923243d6d25b70fae7f4f98d7f8b75e86d2
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-07-23 16:51:04 +0100

    tests with arrays and deep equal comparasions

--- arrays/sum.go
+++ arrays/sum.go
@@ -6,3 +6,29 @@ func Sum(numbers []int) (sum int) {
 	}
 	return sum
 }
+
+// Finds the sum of every array passed in
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// Sums all the numbers in arrays except the first value
+func SumAllTails(numbersToSumTails ...[]int) []int {
+	sums := make([]int, len(numbersToSumTails))
+	for i, arr := range numbersToSumTails {
+		if len(arr) > 1 {
+			sum := Sum(arr[1:])
+			sums[i] = sum
+		} else {
+			sums[i] = 0
+		}
+
+	}
+
+	return sums
+}
