2df3f04de3bee0f01b388ec3f20565e0437a7279
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-07-24 18:32:05 +0100

    test with mocking

---
+++ mocking/mocking.go
@@ -0,0 +1,72 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+type DefaultSleeper struct{}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+
+	fmt.Fprint(out, finalWord)
+}
